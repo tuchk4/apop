1f3591d571e309b1f0846378ace0feb449737fb2
Maksym Bugai <m.bugai@plarium.com>
2017-02-27 14:56:38 +0200

    recursive actions

---
+++ __tests__/recursive.js
@@ -0,0 +1,26 @@
+import rmk from '~/';
+import rmkActions from '~/actions';
+import mock from '../__mock__/recusrive';
+import isArray from 'lodash/isArray';
+
+describe('recursive', () => {
+
+  it('object recursive', () => {
+    let formula = rmk(
+      rmkActions.recursive(
+        [
+          rmkActions.toCamelCase(),
+          rmkActions.update({
+            arrayKeyStr: (localState) =>  (isArray(localState.arrayKey)) ?  localState.arrayKey.join(',') : null
+          }),
+          rmkActions.rename({
+            arrayKeyStr: 'renamedStr'
+          }),
+          rmkActions.clear()
+        ]
+      )
+    );
+    let result = formula(mock.recursiveBefore);
+    expect(result).toEqual(mock.recursiveAfter);
+  });
+});
