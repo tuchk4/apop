2dde055069da306897486400cdb56438825bc058
Valerii Sorokobatko <valeriy.sorokobatko@gmail.com>
2017-03-15 13:43:45 +0200

    update clear test - remove wrong imports - user actions from rmk instance

--- __tests__/clear.js
+++ __tests__/clear.js
@@ -1,18 +1,55 @@
 import rmk from '../lib';
-import rmkActions from '../lib/actions';
-import mock from '../__mock__';
+
+export const before = {
+  "id": 1,
+  "first_name": "Stephen",
+  "last_name": "Nelson",
+  "email": "snelson0@yellowbook.com",
+  "gender": "Male",
+  "ip_address": "74.191.108.58",
+  "city": "Kapotnya",
+  "phrase": "Universal 6th generation portal",
+  "null": null,
+  "undefined": undefined,
+  "date": new Date(2001),
+  "0": 0,
+  "1": 1,
+  "-1": -1,
+  "emptyStr": ""
+};
+
+export const after = {
+  "id": 1,
+  "first_name": "Stephen",
+  "last_name": "Nelson",
+  "email": "snelson0@yellowbook.com",
+  "gender": "Male",
+  "ip_address": "74.191.108.58",
+  "city": "Kapotnya",
+  "phrase": "Universal 6th generation portal",
+  "date": new Date(2001),
+  "0": 0,
+  "1": 1,
+  "-1": -1
+};
 
 describe('Clear action', () => {
   it('Clear object', () => {
-    const formula = rmk(rmkActions.clear());
-    const result = formula(mock.clearDataBefore);
-    expect(result).toEqual(mock.clearDataAfter);
+    const formula = rmk(
+      rmk.clear()
+    );
+
+    const result = formula(before);
+    expect(result).toEqual(after);
   });
 
 
   it('Clear array of objects', () => {
-    const formula = rmk(rmkActions.clear());
-    const result = formula(mock.clearDataBeforeArray);
-    expect(result).toEqual(mock.clearDataAfterArray);
+    const formula = rmk(
+      rmk.clear()
+    );
+
+    const result = formula([before]);
+    expect(result).toEqual([after]);
   });
 });
