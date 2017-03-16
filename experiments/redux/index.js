bdfcb100dbadb3ac9c147197cd17e2b89e2bb473
Valeriy Sorokobatko <valeriy.sorokobatko@gmail.com>
2017-03-16 13:29:15 +0200

    add experiments

---
+++ experiments/redux/index.js
@@ -0,0 +1,27 @@
+import { replace, swap } from 'rmk/shortcuts';
+
+const reducer = (state = {}, action) => {
+  switch action.type {
+
+    'UPDATE':
+      return {
+        ...state,
+        // list: rmk(
+        //   rmk.replace(action.index, action.data)
+        // )(state.list)
+        list: replace(action.index, action.data)(state.list)
+      };
+
+    'SORT':
+      return {
+        ...state,
+        // list: rmk(
+        //   rmk.index(actions.from, action.to)
+        // )(state.list),
+        list: swap(actions.from, action.to)(state.list)
+      };
+
+    default
+      return state;
+  }
+}
