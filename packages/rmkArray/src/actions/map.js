5d51f9d93833e5c27fef2df4ae0664ed2b0c9eb6
Maksym Bugai <m.bugai@plarium.com>
2017-07-12 13:50:39 +0300

    fix

--- packages/rmkArray/src/actions/map.js
+++ packages/rmkArray/src/actions/map.js
@@ -2,7 +2,6 @@
 const map = (...args) =>
   originArr => {
     let callback = args[0];
-    console.log('callback', callback);
     try {
       return originArr.map(callback);
     } catch (err) {console.log('err', err);
