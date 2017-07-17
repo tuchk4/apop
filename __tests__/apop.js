a4c8dc4f7f5c1e1f2c9a56fabc193b1e06d61743
Maksym Bugai <m.bugai@plarium.com>
2017-07-17 17:47:13 +0300

    fix

--- __tests__/apop.js
+++ __tests__/apop.js
@@ -28,7 +28,6 @@ for (let i = 1; i <= 12; i++) {
 }
 
 it('Complex Test 1', () => {
-  console.log('ap.splice(1, 2),', ap.splice(2, 1)([1, 2, 3, 4, 5]));
   const objectFormula = op(
     op.rename({ index: 'id' }),
     op.update({
