563617e117d260da28606dce4b412d972c4ad410
Maksym Bugai <m.bugai@plarium.com>
2017-07-17 17:11:46 +0300

    update op docs

--- src/actions/array/join.js
+++ src/actions/array/join.js
@@ -1,6 +1,6 @@
 /**
  * @memberof ArrayActions
- * @alias ArrayActions.Sort
+ * @alias ap/join
  * @description Join Array
  * @param separator {String} [somebody] Join separator
  * @see {@link https://www.w3schools.com/jsref/jsref_join.asp|w3 schools Array join}
@@ -13,4 +13,5 @@
  * // => '1%2%3%4%5'
  *
  */
-export default (separator = ', ') => originArr => originArr.join(separator);
+const join = (separator = ', ') => originArr => originArr.join(separator);
+export default join;
